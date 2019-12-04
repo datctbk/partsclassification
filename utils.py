7ecb93205357b946772c00ca7f482a38d3702910
Git <aymnemlothe@kings.edu>
2019-12-04 13:03:30 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+import os
+import time
+import urllib
+import numpy as np
+import cv2
+
+from keras.models import load_model
+IMAGE_SIZE = (224,224)
+model = load_model('resnet50custom.h5')
+
+def get_predictions(raw_image):
+	img = img.astype("float")
+	img = img[..., ::-1]
+	img = cv2.resize(img, IMAGE_SIZE)
+	img = img_to_array(img)
+	img = np.expand_dims(img, axis=0)
+	proba = model.predict(img)
+	predictions = path,proba[0]
+    return predictions
